66095e814d5e5c8a3cd31f578d40305b8b2e48ef
Mattia Raffaele Ricciardelli <165891336+MattiaRicciardelli02@users.noreply.github.com>
2024-04-05 11:11:31 +0200

    aggiornamenti

---
+++ search_products/search_products.js
@@ -0,0 +1,18 @@
+
+var stepFilter = 60;
+var Scrolling = true;
+
+
+$(".back_menus").bind("click", function(e){
+    e.preventDefault();
+    $(".filter_wrapper").animate({
+        scrollLeft: "-=" + stepFilter +"px"
+    })
+})
+
+$(".next_menus").bind("click", function(e){
+    e.preventDefault();
+    $(".filter_wrapper").animate({
+        scrollLeft: "+=" + stepFilter +"px"
+    })
+})
