d14350adeb141fae8b785ce1b37e96be0067171c
Marco-Pedicillo <pedicillo.1983285@studenti.uniroma1.it>
2024-04-05 18:20:30 +0200

    home

---
+++ home/home.js
@@ -0,0 +1,17 @@
+let headerBackgrounds = document.querySelectorAll(".background");
+
+let imageIndex = 0;
+
+function changeBackground(){
+    headerBackgrounds[imageIndex].classList.remove("showing");
+
+    imageIndex++;
+
+    if(imageIndex >= headerBackgrounds.length){
+        imageIndex = 0;
+    }
+
+    headerBackgrounds[imageIndex].classList.add("showing");
+}
+
+setInterval(changeBackground, 8000);
