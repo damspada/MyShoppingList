2c62f64350a61b5772f5f26d2d812620d77e58d2
Marco-Pedicillo <pedicillo.1983285@studenti.uniroma1.it>
2024-04-11 18:52:19 +0200

    home

--- home/home_login.js
+++ home/home_login.js
@@ -19,4 +19,25 @@ btnPopup.addEventListener('click', ()=>{
 
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
+    hidePopup();
 });
+
+// function showPopup(){
+//     document.getElementById('overlay').style.display = 'block';
+
+//     document.body.classList.add('active-popup');
+// }
+
+// function hidePopup(){
+//     document.getElementById('overlay').style.display = 'none';
+
+//     document.body.classList.remove('active-popup');
+// }
+
+// document.getElementById('Btn').addEventListener('click', function(){
+//     showPopup();
+// })
+
+// document.getElementById('btn').addEventListener('click', function(){
+//     hidePopup();
+// })
