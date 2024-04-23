e5faf6611a7a86bf8a7c8ea87c3911600c95c659
Marco-Pedicillo <pedicillo.1983285@studenti.uniroma1.it>
2024-04-23 20:19:57 +0200

    home

--- home/home_login.js
+++ home/home_login.js
@@ -1,12 +1,13 @@
 const wrapper=document.querySelector('.wrapper');
+const body = document.querySelector('body');
 const loginLink=document.querySelector('.login-link');
 const registerLink=document.querySelector('.register-link');
 const iconClose= document.querySelector('.icon-close');
 const btnPopup=document.querySelector('.Btn');
 
-
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
+    body.classList.add('popup-active');
 });
 
 loginLink.addEventListener('click', ()=>{
@@ -15,29 +16,28 @@ loginLink.addEventListener('click', ()=>{
 
 btnPopup.addEventListener('click', ()=>{
     wrapper.classList.add('active-popup');
+    body.classList.add('popup-active');
 });
 
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
+    body.classList.remove('popup-active');
     hidePopup();
 });
 
-// function showPopup(){
-//     document.getElementById('overlay').style.display = 'block';
 
-//     document.body.classList.add('active-popup');
-// }
+function openPopup(type) {
+    if (type === 'register') {
+        wrapper.classList.add('active');
+        body.classList.add('popup-active');
+        currentPopup = 'register';
+    } else if (type === 'login') {
+        wrapper.classList.remove('active');
+        body.classList.remove('popup-active');
+        currentPopup = 'login';
+    }
+}
 
-// function hidePopup(){
-//     document.getElementById('overlay').style.display = 'none';
 
-//     document.body.classList.remove('active-popup');
-// }
 
-// document.getElementById('Btn').addEventListener('click', function(){
-//     showPopup();
-// })
 
-// document.getElementById('btn').addEventListener('click', function(){
-//     hidePopup();
-// })
