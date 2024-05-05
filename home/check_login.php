50e4515667208976ee6a1a04e092d9243b817e0d
Marco-Pedicillo <pedicillo.1983285@studenti.uniroma1.it>
2024-05-05 17:59:26 +0200

    roba

--- home/check_login.php
+++ home/check_login.php
@@ -1,4 +1,7 @@
 <?php
+// Inizio della sessione
+session_start();
+
 // Connessione al database
 $servername = "127.0.0.1";
 $username = "root";
@@ -18,12 +21,14 @@ if(isset($_POST['email']) && isset($_POST['pass'])){
     $email = $_POST['email'];
     $pass = $_POST['pass'];
 
-    // Esegue una query per verificare le credenziali dell'utente nel database
+    // Esegui una query per verificare le credenziali dell'utente nel database
     $sql = "SELECT * FROM users WHERE email = '$email' AND pass = '$pass'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // L'utente esiste nel database e le credenziali sono valide
+        // Memorizza i dettagli dell'utente nella sessione
+        $_SESSION['user_details'] = $result->fetch_assoc();
         echo "success";
     } else {
         // L'utente non esiste nel database o le credenziali non sono valide
@@ -32,7 +37,6 @@ if(isset($_POST['email']) && isset($_POST['pass'])){
 } else {
     echo "I dati inviati non sono validi.";
 }
-
 
 // Chiudi la connessione al database
 $conn->close();
