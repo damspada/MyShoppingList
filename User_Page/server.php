5db49ae9391bc01940a09a144bd3dd7745844cc7
Mattia Raffaele Ricciardelli <165891336+MattiaRicciardelli02@users.noreply.github.com>
2024-05-06 17:51:03 +0200

    boh

--- User_Page/server.php
+++ User_Page/server.php
@@ -12,41 +12,60 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
 // Prendi i valori inviati dal form
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$birthdate = $_POST['birthdate'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$lifestyle = $_POST['stile_di_vita'];
-$maxamount = $_POST['importo'];
-$password = $_POST['password'];
-$ID=$_POST['id'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $birthdate = $_POST['birthdate'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $lifestyle = $_POST['stile_di_vita'];
+    $maxamount = $_POST['importo'];
+    $password = $_POST['password'];
+    $ID=$_POST['id'];
 
-// Caricamento dell'immagine
-$target_dir = "C:/xampp/htdocs/uploads/"; // Directory di destinazione per il caricamento dell'immagine
-$target_file = $target_dir . basename($_FILES["avatar-upload"]["name"]);
+    // Caricamento dell'immagine
+    $target_dir = "C:/xampp/htdocs/uploads/";
+    $target_file = $target_dir . basename($_FILES["avatar-upload"]["name"]);
 
-// Controlla se si è verificato un errore durante il caricamento del file
-if ($_FILES["avatar-upload"]["error"] !== UPLOAD_ERR_OK) {
-    echo "Si è verificato un errore durante il caricamento del file: " . $_FILES["avatar-upload"]["error"];
-} else {
-    // Se tutto è ok, prova a caricare il file
-    if (move_uploaded_file($_FILES["avatar-upload"]["tmp_name"], $target_file)) {
-        echo "Il file ". htmlspecialchars( basename( $_FILES["avatar-upload"]["name"])). " è stato caricato.";
+    // Controlla se si è verificato un errore durante il caricamento del file
+    if ($_FILES["avatar-upload"]["error"] !== UPLOAD_ERR_OK) {
+        echo "Si è verificato un errore durante il caricamento del file: " . $_FILES["avatar-upload"]["error"];
     } else {
-        echo "Si è verificato un errore durante il caricamento del file.";
-    }
+        // Se tutto è ok, prova a caricare il file
+        if (move_uploaded_file($_FILES["avatar-upload"]["tmp_name"], $target_file)) {
+            echo "Il file ". htmlspecialchars( basename( $_FILES["avatar-upload"]["name"])). " è stato caricato.";
+        } else {
+            echo "Si è verificato un errore durante il caricamento del file.";
+        }
 
-    // Query per inserire i dati nella tabella del database
-    $sql = "INSERT INTO utenti (id,immagine, email, pass, nome, cognome, born, cell, life, budget)
-    VALUES ('$ID','$target_file', '$email', '$password', '$name', '$surname', '$birthdate', '$phone', '$lifestyle', '$maxamount')";
+        // Query per inserire i dati nella tabella del database
+        $sql = "INSERT INTO utenti (id,immagine, email, pass, nome, cognome, born, cell, life, budget)
+        VALUES ('$ID','$target_file', '$email', '$password', '$name', '$surname', '$birthdate', '$phone', '$lifestyle', '$maxamount')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
+}
+
+
+
+if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET["email"])){
+    $email=$_GET["email"];
+
+    $sql="SELECT * FROM utenti where email='$email'";
+    $result=$conn->query($sql);
+
+    $utente = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $utente[] = $row;
+        }
+    }
+    echo json_encode($utente);
 }
 
 $conn->close();
