9d68604930c12626eadaca390ccc9703d4838a19
DISASTERSURGEON <corsale.1985903@studenti.uniroma1.it>
2024-07-31 11:28:06 +0200

    miglioramento recensioni.php

--- home/recensioni.php
+++ home/recensioni.php
@@ -13,26 +13,7 @@ if ($conn->connect_error) {
     die("Connessione al database fallita: " . $conn->connect_error);
 }
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = isset($_POST['email']) ? $_POST['email'] : 'No session ID set';
-    $text = $_POST['text'];
-
-    // Concatenazione di email e text
-    $fullText = $email . ": " . $text;
-
-    // Preparazione della query per evitare SQL injection
-    $stmt = $conn->prepare("INSERT INTO recensioni (email, text) VALUES (?, ?)");
-    $stmt->bind_param("ss", $email, $fullText);
-
-    if ($stmt->execute()) {
-        echo "Recensione aggiunta con successo!";
-    } else {
-        echo "Errore: " . $stmt->error;
-    }
-
-    $stmt->close();
-} else {
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
     $sql = "SELECT email, text FROM recensioni";
     $result = $conn->query($sql);
 
@@ -44,6 +25,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     echo json_encode($reviews);
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    parse_str(file_get_contents("php://input"), $data);
+
+    $email = isset($_POST['email']) ? $_POST['email'] : 'No session ID set';
+    $text = $_POST['text'];
+
+    // Concatenazione di email e text
+    $fullText = $email . ": " . $text;
+
+    if(isset($data['add']) && $data['add']){
+        // Preparazione della query per evitare SQL injection
+        $stmt = $conn->prepare("INSERT INTO recensioni (email, text) VALUES (?, ?)");
+        $stmt->bind_param("ss", $email, $fullText);
+
+        if ($stmt->execute()) {
+            echo "Recensione aggiunta con successo!";
+        } else {
+            echo "Errore: " . $stmt->error;
+        }
+    }
+
+    // Verifica se è stata effettuata una richiesta MODIFICA
+    if (isset($data['mod']) && $data['mod']) {
+        
+        // modifica vecchia recensione nel database
+        $stmt = $conn->prepare("UPDATE recensioni SET text = ? WHERE email = ?");
+        $stmt->bind_param("ss", $fullText,$email);
+
+        if ($stmt->execute()) {
+            echo "Recensione eliminata con successo!";
+        } else {
+            echo "Errore: " . $stmt->error;
+        }
+
+    }
+
+    $stmt->close();
+} 
 
 // Verifica se è stata effettuata una richiesta DELETE
 if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
